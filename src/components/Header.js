62856cc76d0388c3c8cf51f36f2c85b324431dbf
Selorm Ayornu <95534257+yoselorm@users.noreply.github.com>
2025-09-26 00:21:59 +0000

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,14 +12,14 @@ const Header = () => {
           
           <div className="flex items-center space-x-4">
             {/* Search */}
-            <div className="relative">
+            {/* <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
               <input
                 type="text"
                 placeholder="Search..."
                 className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent w-64"
               />
-            </div>
+            </div> */}
             
             {/* Notifications */}
             <button className="p-2 hover:bg-gray-50 rounded-lg">
